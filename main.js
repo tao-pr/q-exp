dcfcbfa7d1d71e367bbdb29f64aa0309780660c5
Tao (StarColon) <tao@starcolon.com>
2016-02-17 15:07:43 +0700

    ADD INITIAL MAIN.JS

---
+++ main.js
@@ -0,0 +1,23 @@
+/**
+ * Simple Q-learning library for JavaScript ninja
+ * @author StarColon Projects
+ */
+
+var ql     = {}
+var _      = require('underscore');
+var colors = require('colors');
+var config = require('./package.json');
+
+/**
+ * Create a new QL course from the settings
+ * @param {Syllabus}
+ */
+ql.newCourse = function(syllabus){
+
+}
+
+ql.saveCourse = function(){}
+ql.loadCourse = function(){}
+
+
+module.exports = ql;
