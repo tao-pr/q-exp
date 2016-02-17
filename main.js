f8aa8d7b3ef7deebbb1271733cd1cb97dc00b9bd
Tao PR <pataoengineer@gmail.com>
2016-02-17 18:35:55 +0700

    {QL} IMPLEMENT STATE SPACE TRAVERSAL

--- main.js
+++ main.js
@@ -34,15 +34,16 @@ ql.loadAgent = function(path){}
  * @param {Array} state
  * @param {String} action
  * @param {Function} reward updater function
+ * @param {Number} initial reward if new state-action to store
  */
-ql.updatePolicy = function(state,action,rewardUpdater){
+ql.__updatePolicy = function(state,action,rewardUpdater,initial){
 	return function(agent){
 		// Register a new state if haven't
 		if (!agent.policy.hasOwnProperty(state)){
 			agent.policy[state] = {}
 			agent.policy[state] = agent.actionset.map(function(a){
 				// TAOTOREVIEW: This could possibly apply prior knowledge
-				return {action: a, reward: 1}
+				return {action: a, reward: initial}
 			})
 		}
 		else{
@@ -61,16 +62,36 @@ ql.updatePolicy = function(state,action,rewardUpdater){
 /**
  * Explore the reward of the next state after applying an action
  * @param {String} current state
- * @param {String} action to take
  */
-ql.rewardOf = function(state,action){
+ql.__rewardOf = function(state){
 
 	return function(agent){
-		// Explore the next state
-		var state_ = agent.func['stateGenerator'](state,action);
-		// Get the reward of the next state
-		var reward_state = agent.func['rewardOfState'](state);
-		return reward_state;
+		return agent.func['rewardOfState'](state);
+	}
+}
+
+
+/**
+ * Determine (predict) the reward we would get 
+ * when perform a particular action on a state 
+ */
+ql.__q = function(state,action){
+	
+	return function(agent){
+		// Do we have the state and action registered in the policy?
+		if (agent.policy.hasOwnProperty(state)){
+			// Yes, we have the state memorised
+			var _act = (agent.policy[state].filter((a) => a.action==action));
+			if (_act.length==0)
+				return Math.random(); // Don't know anything about the action
+			else
+				return _act[0].reward;
+		}
+		else{
+			// We don't know anything about the current state
+			// Guess it based on uniform distribution then
+			return Math.random();
+		}
 	}
 }
 
@@ -78,16 +99,69 @@ ql.rewardOf = function(state,action){
 /**
  * Explore the subsequent states by trying some actions on it
  */
-ql.exploreNext = function(state){
+ql.__exploreNext = function(state){
 	return function(agent){
 		// List all actions and try
 		var rewards = agent.actionset.map(function(a){
-			var r = ql.rewardOf(state,a)(agent);
-			return {action: a, reward: r}
+			// Predict the reward we would get
+			return {action: a, reward: ql.__q(state,a)}
 		})
 
 		// Sort the actions by rewards (higher first)
 		return _.sortBy(rewards,(r) => -r.reward);
+	}
+}
+
+/**
+ * Start a new learning course of the agent
+ * @param {String} initial state
+ * @param {Function} function to determine whether the state is terminal
+ */
+ql.start = function(initState,stopCrit,alpha){
+	return function(agent){
+		return ql.step(initState,stopCrit,alpha=0.01)
+	}
+}
+
+/**
+ * Step to explore the next state
+ * @param {String} current state
+ * @param {Function} stopping criteria function
+ * @param {}
+ */
+ql.step = function(state,stopCrit,alpha){
+	return function(agent){
+
+		// End up at a terminal state?
+		if (stopCrit(state)){
+			// Finish!
+			return Promise.resolve(agent);
+		}
+
+		// Explore the next states
+		var nexts = ql.__exploreNext(initState);
+		
+		// Pick the best action (greedy tithering)
+		var chosen = nexts[0]; // TAOTODO: We may rely on other choices
+		var currentReward = ql.func['rewardOfState'](state);
+		var nextState = agent.func['stateGenerator'](state);
+		var nextReward = ql.func['rewardOfState'](nextState);
+		
+		// Update the state such that
+		// Q(s, a) += alpha * (reward(s,a) + max(Q(s') - Q(s,a))
+		// where
+		// s  : current state
+		// s' : next state
+		var qdiff = alpha * (chosen.reward + nextReward - currentReward);
+		ql.__updatePolicy(
+			state,
+			chosen.action,
+			(r)=>{ r + qdiff },
+			initReward=Math.random()
+		);
+
+		// Proceed to the next step
+		return Promise.resolve(ql.step(nextState,stopCrit,alpha))
 	}
 }
 
