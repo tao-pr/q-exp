e0eb49724e129eb80d1f56887140e79d5c3ae4f2
Tao PR <pataoengineer@gmail.com>
2016-02-17 16:07:17 +0700

    {QL} DRAFT SOME BASIC FUNC

--- main.js
+++ main.js
@@ -3,21 +3,58 @@
  * @author StarColon Projects
  */
 
-var ql     = {}
-var _      = require('underscore');
-var colors = require('colors');
-var config = require('./package.json');
+var ql      = {}
+var Promise = require('bluebird');
+var _       = require('underscore');
+var colors  = require('colors');
+var config  = require('./package.json');
 
 /**
- * Create a new QL course from the settings
- * @param {Syllabus}
+ * Create a new agent with given predefined actionset
  */
-ql.newCourse = function(syllabus){
-
+ql.newAgent = function(actionset){
+	var agent = {}
+	agent.actionset = actionset;
+	agent.policy = {}
+	return Promise.resolve(agent)
 }
 
-ql.saveCourse = function(){}
-ql.loadCourse = function(){}
+ql.saveAgent = function(path){}
+ql.loadAgent = function(path){}
 
+/**
+ * Update the policy from the observation
+ * @param {Array} state
+ * @param {String} action
+ * @param {Function} reward updater function
+ */
+ql.updatePolicy = function(state,action,rewardUpdater){
+	return function(agent){
+		// Register a new state if haven't
+		if (!agent.policy.hasOwnProperty(state)){
+			agent.policy[state] = {}
+			agent.policy[state] = agent.actionset.map(function(a){
+				// TAOTOREVIEW: This could possibly apply prior knowledge
+				return {action: a, reward: 1}
+			})
+		}
+		else{
+			// State exists, update the action reward
+			agent.policy[state].map(function(a){
+				if (a.action==action) return rewardUpdater(a.reward);
+				else return a.reward
+			})
+		}
+		return Promise.resolve(agent)
+	}
+}
+/**
+ * Predict the reward of the next state after applying an action
+ */
+ql.q = function(state,action){
+	// Check whether the state exists?
+	if (agent.policy.hasOwnProperty(state))
+		return 
+}
 
 module.exports = ql;
