e6b28e61aa8f8e2605c962425067ea13459d0123
Tao PR <pataoengineer@gmail.com>
2016-03-27 01:49:15 +0700

    {STATE} Draft #state class

--- state.js
+++ state.js
@@ -4,8 +4,35 @@
  */
 
 class State{
-	constructor(nParams=1){
-		this.nParams = nParams;
+	/**
+	 * @param {Array} of state parameters
+	 */
+	constructor(xs){
+		this.xs = []
+	}
+
+	static newZeroState(nParams){
+		xs = new Array(nParams).fill(0);
+		s  = new State(xs);
+		return s
+	}
+
+	static eqls(s1,s2){
+		return s1.hash()===s2.hash()
+	}
+
+	/**
+	 * Get a hash-able string of the state
+	 */
+	get hash(){
+		return this.xs.join(',')
+	}
+
+	/**
+	 * Update the state
+	 */
+	set state(xs){
+		this.xs = xs.slice()
 	}
 }
 
