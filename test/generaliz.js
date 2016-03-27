a9c7114bd2121f492c62b8a5489d371bc846c4fb
starcolon <tao@starcolon.com>
2016-03-28 00:23:00 +0700

    Draft test case

--- test/generaliz.js
+++ test/generaliz.js
@@ -6,3 +6,25 @@
 
 var Gen   = require('../generaliz.js');
 var State = require('../state.js');
+
+// Test dataset
+var S = [
+	new State([1,5,3]),
+	new State([2,5,4]),
+	new State([2,5,7]),
+	new State([1,2,3]),
+	new State([0,3,2]),
+	new State([2,2,1])
+]
+var R = [
+	3,
+	3,
+	10,
+	10,
+	3,
+	0
+]
+
+const maxIters = 20;
+const alpha = 0.2;
+Gen.fit(S,R,maxIters,alpha);
